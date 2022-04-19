dd7297a06d046d5fd4f5a836e3c47d21a4afc2f7
Aer-osa <eric.n.long@gmail.com>
2022-04-19 16:23:48 -0700

    Solve

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(...args) {
+    const array = args[0]
+    return array.filter(val => !args.includes(val))
+    };
+    console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
